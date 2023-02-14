6e51d5f7627fb56b7d94e853fd4294e20aa9a8d8
bhoretskyi <bhoretskyi@gmail.com>
2023-02-14 12:00:28 +0100

    1
    Начинає 3 дз

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientList = ingredients.map((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("item");
+  listEl.textContent = 'ingredient';
+  return listEl;
+});
+console.log(ingredientList);
+
+const ingredientElList = document.querySelector('#ingredients');
+ingredientElList.append(...ingredients);
+
