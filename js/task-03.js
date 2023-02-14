23f87258e517247b0b88cadbeafff1551e893e72
bhoretskyi <bhoretskyi@gmail.com>
2023-02-14 13:42:16 +0100

    Проблема з алт

--- js/task-03.js
+++ js/task-03.js
@@ -1,16 +1,32 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+const gallery = document.querySelector(".gallery");
 
-const imageList = images.map
+const imageList = images.map((image) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("list");
+
+  const imageEl = document.createElement("img");
+  imageEl.src = image.url;
+  imageEl.alt = image.alt;
+  imageEl.width = 480;
+  
+
+  
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<li><img src = ${imageEl.src} alt = ${imageEl.alt} width = ${imageEl.width}</li>`
+  );
+});
