331ff6b9db2a5e032a0944470e2d6d55b0520ede
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-02-16 12:24:08 +0100

    Скінчив 9 дз

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  button: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  span: document.querySelector("span"),
+};
+
+refs.button.addEventListener("click", onColorChange);
+
+function onColorChange() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor();
+}
