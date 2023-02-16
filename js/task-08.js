163096d8bea7dc408bcef7c3935b9f6ff1a5e654
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-02-16 11:33:43 +0100

    Скінчив 8 дз

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const mailFormEl = event.currentTarget.elements.email.value;
+  const passwordFormEl = event.currentTarget.elements.password.value;
+  if (mailFormEl === "") {
+    alert("всі поля повинні бути заповнені");
+  } else if (passwordFormEl === "") {
+    alert("всі поля повинні бути заповнені");
+  }
+
+  const formData = {
+    mailFormEl,
+    passwordFormEl,
+  };
+  console.log(formData);
+  form.reset();
+}
